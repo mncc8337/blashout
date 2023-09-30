f53d12c3fb1c2f2c771835d9fc4d93b708fcb76c
mncc8337 <mncc8327@gmail.com>
2023-09-30 15:25:30 +0700

    smoothier movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 10.0
+const SPEED = 100.0
+const FRICTION = 0.1
 
 func _physics_process(delta):
 	if Input.is_action_pressed("up"):
@@ -11,5 +12,7 @@ func _physics_process(delta):
 		velocity.x -= SPEED
 	if Input.is_action_pressed("right"):
 		velocity.x += SPEED
+	
+	velocity *= 1 - FRICTION
 
 	move_and_slide()
