bc9765eb74b552afcafac3aac6fe226732b793dd
mncc8337 <mncc8327@gmail.com>
2023-10-01 12:49:20 +0700

    new class roach

--- scripts/main.gd
+++ scripts/main.gd
@@ -10,6 +10,7 @@ var rng = RandomNumberGenerator.new()
 var foe_attack_dmg_max:float = 10
 var foe_speed_max:float = 600.0
 var foe_health_max:float = 100
+var foe_attack_cooldown_max:float = 1.5
 
 enum FOE_CLASS {RANDOM, BIGASS, ROACH}
 
@@ -25,32 +26,35 @@ func _ready():
 	$difficulty_timer.timeout.connect(increase_diff)
 
 func increase_diff():
-	$foe_spawn_timer.wait_time *= 0.9
+	$foe_spawn_timer.wait_time *= 0.95
 	foe_attack_dmg_max *= 1.1
 	foe_speed_max *= 1.1
 	foe_health_max *= 1.1
+	foe_attack_cooldown_max *= 0.95
 
 func spawn_foe():
 	var foe_instance = foe_model.instantiate()
 	var angle = rng.randf_range(-PI, PI)
 	var dir = Vector2(cos(angle), sin(angle)) * 200 + $player.position
 	foe_instance.position = dir
+
 	var foe_class = rng.randi_range(0, 2)
 	if foe_class == FOE_CLASS.RANDOM:
 		foe_instance.attack_dmg = foe_attack_dmg_max * rng.randf_range(0.5, 1)
 		foe_instance.SPEED = foe_speed_max * rng.randf_range(0.5, 1)
 		foe_instance.max_health = foe_health_max * rng.randf_range(0.5, 1)
-		print("spawned a random foe")
+		foe_instance.attack_cooldown = foe_attack_cooldown_max * rng.randf_range(1, 1.5)
 	elif foe_class == FOE_CLASS.BIGASS:
-		foe_instance.attack_dmg = foe_attack_dmg_max * rng.randf_range(0.9, 1.3)
-		foe_instance.SPEED = foe_speed_max * rng.randf_range(0.1, 0.4)
-		foe_instance.max_health = foe_health_max * rng.randf_range(0.9, 1.7)
-		print("spawned a big ass foe")
+		foe_instance.attack_dmg = foe_attack_dmg_max * rng.randf_range(0.9, 2)
+		foe_instance.SPEED = foe_speed_max * rng.randf_range(0.4, 0.7)
+		foe_instance.max_health = foe_health_max * rng.randf_range(1.1, 2)
+		foe_instance.attack_cooldown = foe_attack_cooldown_max * rng.randf_range(1, 2)
 	elif foe_class == FOE_CLASS.ROACH:
-		foe_instance.attack_dmg = foe_attack_dmg_max * rng.randf_range(0.3, 0.7)
+		foe_instance.attack_dmg = foe_attack_dmg_max * rng.randf_range(0.3, 0.5)
 		foe_instance.SPEED = foe_speed_max * rng.randf_range(1.5, 2)
 		foe_instance.max_health = foe_health_max * rng.randf_range(0.3, 0.7)
-		print("spawned a roach foe")
+		foe_instance.attack_cooldown = foe_attack_cooldown_max * rng.randf_range(0.3, 0.5)
+
 	$foes.add_child(foe_instance)
 
 func show_death_screen():
